a1d45f4de7351ea41531099e7b260561db522a6a
Mark Dueck <mark.dueck@austec-automation.com.au>
2024-11-09 07:21:06 +1000

    Add script to remove duplicate catchall records
    This will eventually move somewhere else

--- test/remove_duplicate_catchall.pl
+++ test/remove_duplicate_catchall.pl
@@ -71,8 +71,6 @@ die "no config available!\n" if (ref($config) ne "HASH"
 
 # log to stderr
 my $logfile = undef; #$config->{'<nmis_logs>'} . "/cli.log"; # shared by nmis-cli and this one
-# my $error = NMISNG::Util::setFileProtDiag(file => $logfile) if (-f $logfile);
-# warn "failed to set permissions: $error\n" if ($error);
 
 # use debug or configured log_level
 my $logger = NMISNG::Log->new( level => NMISNG::Log::parse_debug_level(
@@ -82,11 +80,70 @@ my $logger = NMISNG::Log->new( level => NMISNG::Log::parse_debug_level(
 # now get us an nmisng object, which has a database handle and all the goods
 my $nmisng = NMISNG->new(config => $config, log  => $logger);
 
+# do the work
 $logger->info("Starting remove_catchall_duplicates dryrun=$dryrun");
-my $result = remove_catchall_duplicates();
-
+my $result = remove_catchall_duplicates_using_cluster_id();
 $logger->info("Finished, dryrun=$dryrun, removed records:".$result->{removed_records});
+# done work
 
+
+sub remove_catchall_duplicates_using_cluster_id {
+	my $result;
+	# find all catchalls, group them by node uuid, push information needed to remove duplicates, count matches
+	# and only return where count > 1 (so there were duplicates)
+	my ($entries, undef, $error) = NMISNG::DB::aggregate(
+    collection => $nmisng->inventory_collection,
+    pre_count_pipeline => [
+      { '$match' => { 'concept' => 'catchall' }},
+            { 
+            	'$group' => { '_id' => { 'node_uuid' => '$node_uuid'}, 
+            	'info' => { '$push' => { 'id' => '$_id', 'node_name' => '$node_name', 'cluster_id' => '$cluster_id' }},
+            	'count' => { '$sum' => 1 }}
+            },
+            { '$match' => { 'count' => {'$gt' =>1 }}}
+     ],
+    allowtempfiles => 1
+	);
+	die 'error:'.$error if($error);
+	my $toret = { total => 0, duplicate_names => {}, duplicate_uuids => {}, removed_records => 0 };
+	
+	foreach my $entry (@$entries) {
+		my $node_uuid = $entry->{_id}{node_uuid};
+		my $nodeobj = $nmisng->node(uuid => $node_uuid);
+		my $node_cluster_id = $nodeobj->cluster_id();
+    $toret->{duplicate_uuids}->{$node_uuid}  = 1;
+
+    # info here contains the nnode name, id and cluster id of hte matching catchall document
+    # this list contains all catchalls for the node, just remove the ones that do not match
+    # the cluster_id of the node
+		foreach my $match (@{$entry->{info}}) {
+			my $node_name = $match->{node_name};
+			my $remove_id = $match->{id};
+			$toret->{duplicate_names}->{$node_name}  = 1;
+			if( $match->{cluster_id} ne $node_cluster_id) {
+				$logger->info("Found Duplicate  - node:$node_name remove_id:$remove_id, bad cluster id $match->{cluster_id}");
+			
+				if( !$dryrun ) {
+					my $object_id = NMISNG::DB::make_oid($remove_id);
+	        my $q = NMISNG::DB::get_query( and_part => {_id => $object_id}, no_regex => 1 );
+					my $res = NMISNG::DB::remove( 
+	          collection => $nmisng->inventory_collection(),
+	          query      => $q,
+	        );
+					$logger->error("Deleting of queue entry failed: $res->{error}") if ( !$res->{success} );
+	        $logger->error("Deletion failed: query did not match any records".Dumper($q)) if ( !$res->{removed_records} );
+	        $toret->{removed_records} += $res->{removed_records};
+				} else {
+					$logger->info("NOT deleting, dryrun is on");
+		      $toret->{removed_records}++;
+				}
+			}
+		}	
+	}	
+	return $toret;
+}
+
+# this function is older, and a bit more brutal
 # remove all catchall records for a node if it has duplicates
 # the node will need an update to regenerate it completely
 sub remove_catchall_duplicates {
@@ -129,3 +186,4 @@ sub remove_catchall_duplicates {
 	$toret->{total} = $duplicates;
 	return $toret;
 }
+1;
